940c6ff322c4fbd49d2fbf91ec2ce62b3a544740
Julio Cesar Oliveira <jcn.oliveira@outlook.com>
2020-12-07 14:19:09 -0300

    plot twist, agora usando redshift. Rds free tier estava tomando uma surra, mesmo com set autocommit=0

--- conn.py
+++ conn.py
@@ -1,83 +1,94 @@
 import pymysql.cursors
 import datetime
 import log
+import psycopg2
+
 
 def db_createConnection():
     logger = log.log()
-    
-
     try:
-        # Connect to the database
-        connection = pymysql.connect(host='funcionaldb.cf0vlrdfbqsd.us-east-1.rds.amazonaws.com',
-                                    user='admin',
-                                    password='12345678',
-                                    charset='utf8',
-                                    cursorclass=pymysql.cursors.DictCursor)
+        DB_HOST=''
+        DB_NAME=''
+        DB_PORT=''
+        DB_USER='admin'
+        DB_PWD='12345678'
+        conn_string = "dbname='{}' port='{}' host='{}' user='{}' password='{}'".format(DB_NAME, DB_PORT, DB_HOST, DB_USER, DB_PWD)
+        connection = psycopg2.connect(conn_string)
         logger.debug('connection created')
     except:
-        print("error")
-        pass
+        logger.error('problem on connection')
+
     db_createSchema(connection)
     db_createTables(connection)
     return connection
 
 def db_createTables(connection):
+    logger = log.log()
     try:
-        with connection.cursor() as cursor:
-            sql = """
-            CREATE TABLE IF NOT EXISTS trips(
-                cod INT NOT NULL AUTO_INCREMENT,
-                vendor_id VARCHAR(5),
-                DATE DATE,
-                pickup_datetime TIMESTAMP(6),
-                dropoff_datetime TIMESTAMP(6),
-                passenger_count TINYINT,
-                trip_distance DECIMAL(5, 2),
-                pickup_longitude DECIMAL(10, 10),
-                pickup_latitude DECIMAL(10, 10),
-                rate_code VARCHAR(100),
-                store_and_fwd_flag VARCHAR(100),
-                dropoff_longitude DECIMAL(10, 10),
-                dropoff_latitude DECIMAL(10, 10),
-                payment_type VARCHAR(20),
-                fare_amount DECIMAL(5, 2),
-                surcharge DECIMAL(5, 2),
-                tip_amount DECIMAL(5, 2),
-                tolls_amount DECIMAL(5, 2),
-                total_amount DECIMAL(5, 2),
-                UNIQUE KEY `trip_key`(`cod`, `date`)
-            ) PARTITION BY RANGE(YEAR(DATE))(
-                PARTITION p0
-            VALUES LESS THAN(2010),
-            PARTITION p1
-            VALUES LESS THAN(2011),
-            PARTITION p2
-            VALUES LESS THAN(2012),
-            PARTITION p3
-            VALUES LESS THAN MAXVALUE
-            )"""
-            cursor.execute(sql)
+        logger.debug('creating tables')
+        cursor = connection.cursor()
+        sql = """
+        CREATE TABLE IF NOT EXISTS funcional.trips(
+            cod INT IDENTITY(1,1),
+            vendor_id VARCHAR(5),
+            pickup_datetime TIMESTAMPTZ,
+            dropoff_datetime TIMESTAMPTZ,
+            passenger_count INT,
+            trip_distance NUMERIC(5, 2),
+            pickup_longitude NUMERIC(9, 6),
+            pickup_latitude NUMERIC(9, 6),
+            rate_code VARCHAR(100),
+            store_and_fwd_flag VARCHAR(100),
+            dropoff_longitude NUMERIC(9, 6),
+            dropoff_latitude NUMERIC(9, 6),
+            payment_type VARCHAR(20),
+            fare_amount NUMERIC(5, 2),
+            surcharge NUMERIC(5, 2),
+            tip_amount NUMERIC(5, 2),
+            tolls_amount NUMERIC(5, 2),
+            total_amount NUMERIC(5, 2)
+        );
+        """
+        cursor.execute(sql)
+        logger.debug('trips table created')
+
+        sql = """
+        CREATE TABLE IF NOT EXISTS funcional.vendor(
+            vendor_id VARCHAR(5),
+            name VARCHAR(60),
+            address VARCHAR(60),
+            city VARCHAR(20),
+            state VARCHAR(20),
+            zip VARCHAR(6),
+            country VARCHAR(20),
+            contact VARCHAR(60),
+            current VARCHAR(5)
+        )"""
+        cursor.execute(sql)
+        logger.debug('trips table created')
+        connection.commit()
+        logger.debug('table creation commited')
+
+    except:
+        logger.error('problem on create table process')
+
     finally:
-        #connection.close()
         return True
 
 
 def db_createSchema(connection):
+    logger = log.log()
     try:
-        with connection.cursor() as cursor:
-            sql = "CREATE DATABASE IF NOT EXISTS dbfuncional"
-            cursor.execute(sql) #, ('webmaster@python.org', 'very-secret'))
+        cursor = connection.cursor()
+        sql = "CREATE SCHEMA IF NOT EXISTS funcional;"
+        cursor.execute(sql)
+        logger.debug('funcional schema created')
+        connection.commit()
+        logger.debug('schema creation commited')
 
-#        # connection is not autocommit by default. So you must commit to save
-#        # your changes.
-#        connection.commit()
-#
-#        with connection.cursor() as cursor:
-#            # Read a single record
-#            sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-#            cursor.execute(sql, ('webmaster@python.org',))
-#            result = cursor.fetchone()
-#            print(result)
+    except:
+        logger.error('problem on create schema process')
+
     finally:
         return True
 
@@ -85,5 +96,8 @@ def db_query(connection, sql):
     try:
         with connection.cursor() as cursor:
             cursor.execute(sql) #, ('webmaster@python.org', 'very-secret'))
-    finally:
-        return True
+            result = cursor.fetchone()
+            return result
+    except:
+        logger.error('problem on query')
+
--- dataimport.py
+++ dataimport.py
@@ -6,6 +6,7 @@ import log
 from datetime import datetime
 
 
+
 def lambda_handler(event, context):
     logger = log.log()
     logger.debug('starting')
@@ -13,37 +14,38 @@ def lambda_handler(event, context):
 
     connection = conn.db_createConnection()
     conn.db_query(connection, "START TRANSACTION;")
+    logger.debug('starting transaction')
+
 
     for path, dirnames, filenames in os.walk('data/'):
         if path == "data/":
-            filenames = ["teste.json"]
+            filenames = ["data-sample_data-nyctaxi-trips-2009-json_corrigido.json"]
             for x in filenames:
+                logger.debug('sending ' + x)
+
                 with open(path+x) as json_file:
                     for p in json_file:
                         data = json.loads(p)
-                        data["date"] = "10/10/2020"
+                        
                         data["pickup_datetime"] = datetime.strptime(data["pickup_datetime"], '%Y-%m-%dT%H:%M:%S.%f+00:00')
                         data["dropoff_datetime"] = datetime.strptime(data["dropoff_datetime"], '%Y-%m-%dT%H:%M:%S.%f+00:00')
-                        sql = """INSERT INTO trips (vendor_id, date, pickup_datetime, dropoff_datetime, passenger_count, trip_distance,
+                        data["date"] = data["pickup_datetime"].strftime('%Y-%m-%d')
+                        sql = """INSERT INTO dbfuncional.trips (vendor_id, date, pickup_datetime, dropoff_datetime, passenger_count, trip_distance,
                                                     pickup_longitude, pickup_latitude, rate_code, store_and_fwd_flag, dropoff_longitude, 
                                                     dropoff_latitude, payment_type, fare_amount, surcharge, tip_amount, tolls_amount, total_amount)
                                  VALUES 
-                                    ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})
-                                    """.format(data["vendor_id"], data["date"], data["pickup_datetime"], data["dropoff_datetime"], data["passenger_count"], data["trip_distance"],
-                                               data["pickup_longitude"], data["pickup_latitude"], data["rate_code"], data["store_and_fwd_flag"], data["dropoff_longitude"], 
-                                               data["dropoff_latitude"], data["payment_type"], data["fare_amount"], data["surcharge"], data["tip_amount"], data["tolls_amount"], data["total_amount"])
+                                    ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');
+                                    """.format(str(data["vendor_id"]), data["date"], data["pickup_datetime"], data["dropoff_datetime"], data["passenger_count"], data["trip_distance"],
+                                               data["pickup_longitude"], data["pickup_latitude"], str(data["rate_code"]), str(data["store_and_fwd_flag"]), data["dropoff_longitude"], 
+                                               data["dropoff_latitude"], str(data["payment_type"]), data["fare_amount"], data["surcharge"], data["tip_amount"], data["tolls_amount"], data["total_amount"])
                         print(sql.strip())
-
-
-                        #for key, value in data.items() :
-                        #    print (key, type(value))
-                        #print("---------")
-
+                        conn.db_query(connection, sql)
         if path == "data/lookups":
             print(path)
             print(filenames)
-    conn.db_query(connection, "COMMIT")
-
+    connection.commit()
+    #conn.db_query(connection, "COMMIT")
+    logger.debug('commited')
 #
     return {
         'statusCode': 200,
